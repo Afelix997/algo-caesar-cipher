9dd716277424160d0ef9e3937abed22e00685596
afelix997 <afelix2013@gmail.com>
2022-06-01 20:29:15 -0700

    answer

--- python/caesar_cipher.py
+++ python/caesar_cipher.py
@@ -1,1 +1,13 @@
-def caesar_cipher(string, shift_amount):
+def caesar_cipher(str, shift):
+    alpha= 'abcdefghijklmnopqrstuvwxyz'
+    upper_alpha= list((alpha *2).upper())
+    alpha= list(alpha *2)
+    str_list= list(str)
+
+    for i, letter in enumerate(str_list):
+        if letter.isalpha() and letter.isupper():
+            str_list[i] = upper_alpha[upper_alpha.index(str_list[i]) + shift] 
+        elif letter.isalpha():
+            str_list[i] = alpha[alpha.index(str_list[i]) + shift]
+  
+    return ''.join(str_list)
